2bddb898a057870ea87f35ec4e8ba5a1b631f81c
jeanl <jeanl@DESKTOP-V2AQP4K>
2023-06-29 19:50:05 -0300

    Adicionar arquivos de projeto.

---
+++ CRUDProdutos/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("CRUDProdutos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CRUDProdutos")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// É possível especificar todos os valores ou definir como padrão os números de revisão e de versão
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
